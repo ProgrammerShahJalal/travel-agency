41d8adb9eebc50bcd342a0d246eb00c4ebbe7d65
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-01-26 13:39:23 +0600

    features add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
 import Hero from '../Hero/Hero';
+import Features from '../Features/Features';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Features />
         </div>
     );
 };

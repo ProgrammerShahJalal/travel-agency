8bf93610ae201459b48de4ebbf16516f191f92cb
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-01-26 12:45:53 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import Hero from '../Hero/Hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+        </div>
+    );
+};
+
+export default Home;

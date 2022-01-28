01332e175527f6b234a9d8c629fd00858c90413e
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-01-28 18:23:31 +0600

    data loading animation

--- src/components/Blogs/Blog/Blog.js
+++ src/components/Blogs/Blog/Blog.js
@@ -4,7 +4,14 @@ const Blog = ({ blog }) => {
     const { title, img, expense, rating } = blog;
     return (
         <div>
-            <h2>{title}</h2>
+            <div className="md:shrink-0 rounded-xl shadow-lg">
+                <img className="h-48 w-full object-cover md:h-full md:w-48 rounded-md" src={img} alt='' />
+                <h2 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold truncate">{title}</h2>
+                <div className='flex justify-around items-center'>
+                    <p className="mt-2 text-slate-500">${expense}</p>
+                    <p className="mt-2 text-slate-500">{rating}</p>
+                </div>
+            </div>
         </div>
     );
 };
